abca27ef14d852192eaf47e1d4decff5d93c4761
fasty86 <oleg.tolm1986@gmail.com>
2023-04-16 13:44:01 +0300

    feat: finish task

--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,51 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js")
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date = null) {
+  const SEASONS = ["winter", "spring", "summer", "autumn"]
+  if (!date) return `Unable to determine the time of year!`
+  if (
+    Object.prototype.toString.call(date) != "[object Date]" ||
+    !dateIsValid(date)
+  )
+    throw new Error("Invalid date!")
+  // console.log(
+  //   Object.getOwnPropertyNames(Object.getPrototypeOf(new Date())).length !=
+  //     Object.getOwnPropertyNames(Object.getPrototypeOf(date)).length
+  // )
+  if (date instanceof Date) {
+    if (date.getMonth() <= 1 || date.getMonth() == 11) {
+      return SEASONS[0]
+    }
+    if (date.getMonth() <= 4) {
+      return SEASONS[1]
+    }
+    if (date.getMonth() <= 7) {
+      return SEASONS[2]
+    }
+    return SEASONS[3]
+  } else throw new Error(`Invalid date!`)
 }
-
+function dateIsValid(date) {
+  const propAll = Object.getOwnPropertyNames(new Date())
+  const dateProp = Object.getOwnPropertyNames(date)
+  let res = dateProp.every((el) => propAll.includes(el))
+  // console.log(res)
+  if (res) {
+    return true
+  }
+  return false
+}
 module.exports = {
-  getSeason
-};
+  getSeason,
+}
